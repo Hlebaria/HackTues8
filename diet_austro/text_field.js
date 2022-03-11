0104458e1bb5c86708221d16d51b1b9354fa21b4
vladimir.s.barev.2020@elsys-bg.org <vladimir.s.barev.2020@elsys-bg.org>
2022-03-11 20:20:05 +0200

    choose gender done

--- diet_austro/text_field.js
+++ diet_austro/text_field.js
@@ -1,7 +1,12 @@
 var diet_answers = []
+
+for(var i = 0; i < 12; i++){
+    diet_answers[i] = 0
+}
+
 function Answers(questions, answer) {
         diet_answers[questions] = answer
-        if (diet_answers[questions]==0){
+        if (diet_answers[questions]==1){
             console.log("Answer");
         }
 }
