597249af290886616f20b4bdd0ec90a59c1300eb
allekssaa <aleksa.k.rashova.2020@elsys.bg.org>
2022-03-11 14:20:59 +0200

    Trying to put home icon but not successfuly

---
+++ python_window.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+
+def __init__(self):
+    self.root = tk.Tk()
+    self.root.title("Speed Type test APP")
+    self.root.geometry("800x600")
+    self.root.config(background="#BB78E4")
