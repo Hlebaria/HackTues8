2f32e327b21513cee2a249bed6853f4edbf1078b
allekssaa <aleksa.k.rashova.2020@elsys.bg.org>
2022-03-11 13:39:09 +0200

    Adding a text field with javascript

---
+++ diet_austro/text_field.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    var x = document.createElement("INPUT");
+    x.setAttribute("type", "text");
+    x.setAttribute("value", "Type Here...");
+    document.body.appendChild(x);
+  }
