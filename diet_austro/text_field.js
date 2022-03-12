6bdf0544c6457443da1e8c34a3492e0d1c89166c
Hlebaria <mitigr@abv.bg>
2022-03-12 12:22:11 +0200

    removed excess comment and made a check for the diet inputs

--- diet_austro/text_field.js
+++ diet_austro/text_field.js
@@ -5,21 +5,27 @@ function calculate_cal() {
   }
 let choices = [];
 
+
 function Button(){
+  var ok = 0;
   let choices = [11];
-  
-  choices[0] = document.getElementById("Weight").value;
-  choices[1] = document.getElementById("Height").value;
-  choices[2] = document.getElementById("Age").value;
-  choices[3] = document.getElementById("gender").value;
-  console.log(choices[3])
-  choices[4] = document.getElementById("Eggs").value;
-  choices[5] = document.getElementById("Lactose").value;
-  choices[6] = document.getElementById("Fish").value;
-  choices[7] = document.getElementById("Peanuts").value;
-  choices[8] = document.getElementById("Soy").value;
-  choices[9] = document.getElementById("Tree nuts").value;
-  choices[10] = document.getElementById("Wheat").value;
-  console.log(choices)
-    alert("uga buga");
-}
+  var problem = false
+
+      choices[0] = document.getElementById("Weight").value;
+      choices[1] = document.getElementById("Height").value;
+      choices[2] = document.getElementById("Age").value;
+      choices[3] = document.getElementById("gender").value;
+      choices[4] = document.getElementById("Eggs").value;
+      choices[5] = document.getElementById("Lactose").value;
+      choices[6] = document.getElementById("Fish").value;
+      choices[7] = document.getElementById("Peanuts").value;
+      choices[8] = document.getElementById("Soy").value;
+      choices[9] = document.getElementById("Tree nuts").value;
+      choices[10] = document.getElementById("Wheat").value;
+      console.log(choices);
+
+      if(choices[0] == 0 || choices[1] == 0 || choices[2] == 0 || choices[3] == 0){
+       alert("uga buga");
+     }
+     else alert("raboti")
+  }
