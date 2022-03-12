ceb879c964425f4b9d8dd843b06250c8158c137a
vladimir.s.barev.2020@elsys-bg.org <vladimir.s.barev.2020@elsys-bg.org>
2022-03-12 02:13:03 +0200

    Added a check to the input info in diet

--- diet_austro/text_field.js
+++ diet_austro/text_field.js
@@ -7,19 +7,29 @@ let choices = [];
 
 function Button(){
   let choices = [11];
-  
-  choices[0] = document.getElementById("Weight").value;
-  choices[1] = document.getElementById("Height").value;
-  choices[2] = document.getElementById("Age").value;
-  choices[3] = document.getElementById("gender").value;
-  console.log(choices[3])
-  choices[4] = document.getElementById("Eggs").value;
-  choices[5] = document.getElementById("Lactose").value;
-  choices[6] = document.getElementById("Fish").value;
-  choices[7] = document.getElementById("Peanuts").value;
-  choices[8] = document.getElementById("Soy").value;
-  choices[9] = document.getElementById("Tree nuts").value;
-  choices[10] = document.getElementById("Wheat").value;
-  console.log(choices)
-    alert("uga buga");
+  var ok = 1;
+  do{
+    ok=1;
+    choices[0] = document.getElementById("Weight").value;
+    choices[1] = document.getElementById("Height").value;
+    choices[2] = document.getElementById("Age").value;
+    choices[3] = document.getElementById("gender").value;
+    choices[4] = document.getElementById("Eggs").value;
+    choices[5] = document.getElementById("Lactose").value;
+    choices[6] = document.getElementById("Fish").value;
+    choices[7] = document.getElementById("Peanuts").value;
+    choices[8] = document.getElementById("Soy").value;
+    choices[9] = document.getElementById("Tree nuts").value;
+    choices[10] = document.getElementById("Wheat").value;
+    console.log(choices)
+
+    for(var n=0;n<4;n++){
+      if(choices[n] =='null'){
+        alert("Insuficient information, input all the nessessary information");
+        ok=0;
+      }
+    }
+  }while(ok==0)
+
+  alert("Success");
 }
